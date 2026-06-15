a714019154ebbaf6ad92b1cffa3aaa1a8e0a5742
Salunkhe Aditya Sudhakar <adityalpu01@gmail.com>
2026-06-15 05:49:33 +0530

    feat: set up native Node.js HTTP server boilerplate

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('SplitFlat HTTP Server is active\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
